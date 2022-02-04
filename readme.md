90b7b4e25da2188fc8c199fc6af6abeaee3a99cd
ridhoadtyaa <r2603an@gmail.com>
2022-02-04 20:43:51 +0700

    init

---
+++ readme.md
@@ -0,0 +1,3 @@
+### Slicing design with Tailwind CSS
+
+[Design](https://www.figma.com/community/file/1070325217542228760)
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	content: ['./public/*.html'],
+	theme: {
+		extend: {
+			container: {
+				padding: '2rem',
+			},
+		},
+	},
+	plugins: [],
+};
